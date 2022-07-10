02861819d4fa331f8594e9ef48059b186deb1fcc
alingse <alingse@foxmail.com>
2022-07-10 10:46:09 +0800

    use a setting

--- cmd/asasalint/main.go
+++ cmd/asasalint/main.go
@@ -10,12 +10,26 @@ import (
 
 func main() {
 	var extraExclude string
+	var include string
+	var disableDefaultExclude bool
+	var ignoreInTest bool
 	flag.StringVar(&extraExclude,
 		"e",
 		"",
 		"Extra Exclude Func Names, like: FuncA,FuncB,Func",
 	)
+	flag.StringVar(&extraExclude, "i", "",
+		"Must Include Func Names, like: FuncA,FuncB,Func")
+	flag.BoolVar(&disableDefaultExclude, "no-default-exclude", false,
+		"disbale the default exclude func names: "+asasalint.DefaultExclude)
+	flag.BoolVar(&ignoreInTest, "ignore-in-test", false,
+		"ingore case in * _test.go")
 	flag.Parse()
-	excludes := strings.Split(extraExclude, ",")
-	singlechecker.Main(asasalint.NewAnalyzer(excludes, nil))
+
+	setting := asasalint.LinterSetting{
+		Exclude:               strings.Split(extraExclude, ","),
+		Include:               strings.Split(include, ","),
+		DisableDefaultExclude: true,
+	}
+	singlechecker.Main(asasalint.NewAnalyzer(setting))
 }
