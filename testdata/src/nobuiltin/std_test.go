e8ab02430b3e638b958d1d11a24e350f0b26bda3
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-07-16 12:08:00 +0200

    fix: add missing Fprint

--- testdata/src/nobuiltin/std_test.go
+++ testdata/src/nobuiltin/std_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"testing"
 )
@@ -12,6 +13,15 @@ func TestStd(t *testing.T) {
 	fmt.Println(a)      // want `pass \[\]any as any to func Println func\(a \.\.\.any\) \(n int, err error\)`
 	fmt.Printf("%v", a) // want `pass \[\]any as any to func Printf func\(format string, a \.\.\.any\) \(n int, err error\)`
 	fmt.Print(a)        // want `pass \[\]any as any to func Print func\(a \.\.\.any\) \(n int, err error\)`
+
+	fmt.Sprintln(a)      // want `pass \[\]any as any to func Sprintln func\(a \.\.\.any\) string`
+	fmt.Sprintf("%v", a) // want `pass \[\]any as any to func Sprintf func\(format string, a \.\.\.any\) string`
+	fmt.Sprint(a)        // want `pass \[\]any as any to func Sprint func\(a \.\.\.any\) string`
+
+	var w io.Writer
+	fmt.Fprintln(w, a)      // want `pass \[\]any as any to func Fprintln func\(w io.Writer, a \.\.\.any\) \(n int, err error\)`
+	fmt.Fprintf(w, "%v", a) // want `pass \[\]any as any to func Fprintf func\(w io.Writer, format string, a \.\.\.any\) \(n int, err error\)`
+	fmt.Fprint(w, a)        // want `pass \[\]any as any to func Fprint func\(w io.Writer, a \.\.\.any\) \(n int, err error\)`
 
 	log.Println(a)      // want `pass \[\]any as any to func Println func\(v \.\.\.any\)`
 	log.Printf("%v", a) // want `pass \[\]any as any to func Printf func\(format string, v \.\.\.any\)`
